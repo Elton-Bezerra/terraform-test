909d092eebff6d0a7e93495cce3cc4286aba9d48
Elton Bezerra <elton.cin@hotmail.com>
2021-09-13 17:20:02 -0300

    Datasource com query em recursos remotos

--- dependencies/main.tf
+++ dependencies/main.tf
@@ -2,16 +2,33 @@ provider "aws" {
   region = "sa-east-1"
 }
 
-resource "aws_instance" "db" {
+resource "aws_instance" "ec2db" {
   ami           = "ami-09b9b17384f68fd7c"
   instance_type = "t2.micro"
+
+  tags = {
+    "name" = "DB Server"
+  }
 }
 
-resource "aws_instance" "web" {
+resource "aws_instance" "ec2" {
   ami           = "ami-09b9b17384f68fd7c"
   instance_type = "t2.micro"
 
   depends_on = [
-    aws_instance.db
+    aws_instance.ec2db
   ]
 }
+
+data "aws_instances" "dbsearch" {
+  instance_state_names = ["terminated"]
+  instance_tags = {
+    Name = "mysql"
+  }
+
+}
+
+output "dbsearch_output" {
+  value = data.aws_instances.dbsearch.ids
+}
+
